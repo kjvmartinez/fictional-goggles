b6f5f4bf27e79d4aa9532c0800d64cf572db9fd9
Kristine Joy Martinez <kjvmartinez@Kristines-MacBook-Air-5.local>
2022-11-06 14:57:11 +0800

    initial changes

---
+++ custom.js
@@ -0,0 +1,50 @@
+const pokemons = [
+    {
+        name: 'pikachu',
+        hp:100,
+        type:['type'],
+        quick_moves: ['quick attack', 'tail whip', 'tackle'],
+        main_moves: ['thunder shock', 'spark', 'discharge'],
+        vulnerable_to: ['ground'],
+        resistant_to: ['fyling','steel','electric']
+    },
+    {
+        name: 'bulbasaur',
+        hp:95,
+        type:['grass','normal'],
+        quick_moves: ['vine whip', 'tackle'],
+        main_moves: ['speed bomb', 'sludge bomb'],
+        vulnerable_to: ['flying', 'fire', 'psychic','ice'],
+        resistant_to: ['fighting','water','electric','fairy','grass']
+    },
+    {
+        name: 'charmander',
+        hp:110,
+        type:['fire','normal'],
+        quick_moves: ['ember', 'scratch'],
+        main_moves: ['flame thrower', 'flame charge', 'flame burst'],
+        vulnerable_to: ['ground', 'rock', 'water'],
+        resistant_to: ['bug','steel','fire','grass','ice','steel','fairy']
+    },
+    {
+        name: 'squirtle',
+        hp:105,
+        type:['water','normal','ice'],
+        quick_moves: ['tackle', 'bubble'],
+        main_moves: ['aqua jet', 'aqua tail', 'water pulse'],
+        vulnerable_to: ['grass', 'electric'],
+        resistant_to: ['steel','fire','water','ice']
+    },
+    {
+        name: 'eevee',
+        hp:90,
+        type:['normal'],
+        quick_moves: ['quick attack', 'tackle'],
+        main_moves: ['dig'],
+        vulnerable_to: ['fighting'],
+        resistant_to: ['ghost']
+    }
+];
+
+
+
